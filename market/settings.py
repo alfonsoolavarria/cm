e3b48d7f6dc1780eebc12c3f642f1653de5d177d
alfonso-dev <alfonso.olavarria@indagia.com>
2020-04-26 09:37:23 -0400

    uodate db

--- market/settings.py
+++ market/settings.py
@@ -74,31 +74,31 @@ WSGI_APPLICATION = 'market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         #'ENGINE': 'django.db.backends.sqlite3',
-#         #'TIMEOUT':2,
-#         'NAME': 'criollitos',
-#         'USER': 'postgres',
-#         'PASSWORD': '1q2w3e4r',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'djongo',
-            'ENFORCE_SCHEMA': True,
-            'NAME': 'criollotest',
-            'host': '127.0.0.1',
-            'port': 27017,
-            'username': 'root',
-            'password': 'root',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'TIMEOUT':2,
+        'NAME': 'criollitos',
+        'USER': 'postgres',
+        'PASSWORD': '1q2w3e4r',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'djongo',
+#             'ENFORCE_SCHEMA': True,
+#             'NAME': 'criollotest',
+#             'host': '127.0.0.1',
+#             'port': 27017,
+#             'username': 'root',
+#             'password': 'root',
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
