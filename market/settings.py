52da888c86c602d8edebaf69f816ff08cc3e7717
alfonso-dev <alfonso.olavarria@indagia.com>
2020-04-29 08:40:35 -0400

    db

--- market/settings.py
+++ market/settings.py
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         #'TIMEOUT':2,
-        'NAME': 'criollitos',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': 'criollitos',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
