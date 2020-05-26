4e2c8c31a947760dd23ba7079e2f1e29724c6f11
alfonso-dev <alfonso.olavarria@indagia.com>
2020-05-26 10:43:29 -0400

    correccion de header

--- market/settings.py
+++ market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3(%m#=&3u)%dxq$#%8)&5+kyy3(5y0d9dt-f@lvj7!3cgv8$-e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False #en prod False
 
-ALLOWED_HOSTS = ['127.0.0.1','*','192.168.0.6'] #* en prod remove
+ALLOWED_HOSTS = ['127.0.0.1','*'] #* en prod remove
 
 
 # Application definition
